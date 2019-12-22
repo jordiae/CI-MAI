5b50c47d6ddc2901458b41942ea56ce29b6078e7
Jordi Armengol Estapé <me@jordiae.com>
2019-12-22 15:25:05 +0100

    finished exercise 3

--- lab/evo/lab3.m
+++ lab/evo/lab3.m
@@ -21,7 +21,8 @@ varTypes = ["double", "double", "double", "string", "double", "double"];
 sz = [5*4*4*3*6 6];
 results = table('Size', sz, 'VariableTypes', varTypes, 'VariableNames', varNames);
 
-
+record_x = [];
+record_y = [];
 i = 0;
 for p = 1:length(population_sizes)
     population_size = population_sizes(p);
@@ -44,10 +45,21 @@ for p = 1:length(population_sizes)
                     opts = gaoptimset(opts, 'CrossoverFraction', reproduction);
                     rng default % rng (random number generation) for reproducibility
                     total = 0;
+                    best_x = 0;
+                    best_y = 0;
+                    min_ = 99999999999;
                     for j = 1:3
                         [x, fval]=ga(FitnessFunction,2,[],[],[],[],[],[],[],opts);
                         total = total + fval;
+                        if fval < min_
+                            min_ = fval;
+                            best_x = x(1);
+                            best_y = x(2);
+                        end
+                        
                     end
+                    record_x = [record_x; best_x];
+                    record_y = [record_y; best_y];
                     avg = total/3;
                     results(i, :) = {population_size, n_generation, initial_range, selection_name, reproduction, avg};
                 end
@@ -56,3 +68,16 @@ for p = 1:length(population_sizes)
     end
 end
 
+
+T2 = results(:,S);
+a = table2array(T2);
+C = corr(a);
+C_fval = C(:,5); % correlation between numeric variables and fval
+scatter(a(:,4), a(:,5));
+xlabel('Reproduction (crossover vs mutation ratio)')
+ylabel('FVal')
+[val, index] = min(a(:,5)); % val =0.0003479, index = 1255
+results(index,:) % Population = 100, NGenerations = 50, InitialRange = 1.5, Selection =  "selectiontournament", Reproductiin = 0
+record_x(index) % 0.9910
+record_y(index) % 0.9818
+save('results.mat','results','record_x', 'record_y');
